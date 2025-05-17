c52376beed34368e1b8e7e5b3da2670dbf4dcc21
Vinod-V3 <vinod@tunerlabs.com>
2025-05-17 18:41:20 +0530

    Added share and download feature for webview

--- lib/webview_screen.dart
+++ lib/webview_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:share_plus/share_plus.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class WebViewScreen extends StatefulWidget {
   final String url;
@@ -19,6 +25,21 @@ class _WebViewScreenState extends State<WebViewScreen> {
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..loadRequest(Uri.parse(widget.url))
+      ..addJavaScriptChannel(
+        'FlutterChannel',
+        onMessageReceived: (JavaScriptMessage message) {
+          final Map<String, dynamic> data = jsonDecode(message.message);
+          final content = '${data['url']}';
+          final title = '${data['title']}';
+          print("Flutter channel data received: ${data}");
+          if (data['type'] == 'share') {
+            downloadAndSharePdf(content,title);
+            // );
+          }else if(data['type'] == 'download'){
+            downloadFileToDownloads(content, title);
+          }
+        },
+      )
       ..setNavigationDelegate(
         NavigationDelegate(
           onPageFinished: (url){
@@ -69,4 +90,58 @@ class _WebViewScreenState extends State<WebViewScreen> {
       ),
     );
   }
+
+Future<void> downloadAndSharePdf(String url, String filename) async {
+  try {
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode != 200) {
+      throw Exception('Failed to download file');
+    }
+
+    final tempDir = await getTemporaryDirectory();
+    final filePath = '${tempDir.path}/$filename.pdf';
+    final file = File(filePath);
+    await file.writeAsBytes(response.bodyBytes);
+
+    final xFile = XFile(filePath, name: "$filename.pdf", mimeType: 'application/pdf');
+    await Share.shareXFiles([xFile], text: "Sharing PDF: $filename");
+
+  } catch (e) {
+    print("Error downloading or sharing PDF: $e");
+  }
+}
+
+
+Future<void> downloadFileToDownloads(String url, String filename) async {
+  try {
+    // if (Platform.isAndroid) {
+    //   var status = await Permission.storage.request();
+    //   if (!status.isGranted) {
+    //     throw Exception("Storage permission not granted");
+    //   }
+    // }
+
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode != 200) {
+      throw Exception('Failed to download file');
+    }
+
+    Directory? downloadsDir;
+    if (Platform.isAndroid) {
+      downloadsDir = Directory('/storage/emulated/0/Download');
+    } else {
+      downloadsDir = await getApplicationDocumentsDirectory();
+    }
+
+    final filePath = '${downloadsDir.path}/$filename.pdf';
+    final file = File(filePath);
+
+    await file.writeAsBytes(response.bodyBytes);
+
+    print("File saved to: $filePath");
+  } catch (e) {
+    print("Error saving file: $e");
+  }
+}
+
 }

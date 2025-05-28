cd66263d3547e77efc9a3da89adc2e06adb69de1
Vinod-V3 <vinod@tunerlabs.com>
2025-05-29 00:43:58 +0530

    flutter_inappwebview package changes

--- lib/webview_screen.dart
+++ lib/webview_screen.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:share_plus/share_plus.dart';
 import 'dart:io';
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:share_plus/share_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class WebViewScreen extends StatefulWidget {
@@ -17,131 +17,122 @@ class WebViewScreen extends StatefulWidget {
 }
 
 class _WebViewScreenState extends State<WebViewScreen> {
-  late final WebViewController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse(widget.url))
-      ..addJavaScriptChannel(
-        'FlutterChannel',
-        onMessageReceived: (JavaScriptMessage message) {
-          final Map<String, dynamic> data = jsonDecode(message.message);
-          final content = '${data['url']}';
-          final title = '${data['title']}';
-          print("Flutter channel data received: ${data}");
-          if (data['type'] == 'share') {
-            downloadAndSharePdf(content,title);
-            // );
-          }else if(data['type'] == 'download'){
-            downloadFileToDownloads(content, title);
-          }
-        },
-      )
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageFinished: (url){
-            _setTokenInWebView();
-          }
-        )
-      );
-  }
-
-    void _setTokenInWebView() {
-      _controller.runJavaScript("""
-        localStorage.setItem('headers',JSON.stringify({
-          "authorization": "Bearer <value>",
-          "x-auth-token": "<value>",
-          "x-authenticated-user-token": "<value>",
-          "x-channel-id": "<value>",
-          "x-device-id": "<value>",
-          "x-session-id": "<value>"
-        }));
-        localStorage.setItem('name', <value>);
-        localStorage.setItem('accToken',<value>);
-        localStorage.setItem('profileData', JSON.stringify({
-          "state": "<value>",
-          "cluster": "<value>",
-          "district": "<value>",
-          "block": "<value>",
-          "school": "<value>",
-          "role": "<value>"
-        }));
-      """);
-  }
-
-  Future<bool> _onWillPop() async {
-    if (await _controller.canGoBack()) {
-      _controller.goBack();
-      return false;
-    }
-    return true;
-  }
+  late InAppWebViewController _webViewController;
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: _onWillPop,
+      onWillPop: () async {
+        if (await _webViewController.canGoBack()) {
+          _webViewController.goBack();
+          return false;
+        }
+        return true;
+      },
       child: Scaffold(
         appBar: AppBar(title: const Text("WebView")),
-        body: WebViewWidget(controller: _controller),
+        body: InAppWebView(
+          initialUrlRequest: URLRequest(url: WebUri(widget.url)),
+          initialSettings: InAppWebViewSettings(
+            javaScriptEnabled: true,
+            allowsInlineMediaPlayback: true,
+            mediaPlaybackRequiresUserGesture: false,
+          ),
+          onWebViewCreated: (controller) {
+            _webViewController = controller;
+
+            _webViewController.addJavaScriptHandler(
+              handlerName: 'FlutterChannel',
+              callback: (args) async {
+                final data = args[0];
+                final content = data['url'];
+                final title = data['title'];
+
+                if (data['type'] == 'share') {
+                  await downloadAndSharePdf(content, title);
+                } else if (data['type'] == 'download') {
+                  await downloadFileToDownloads(content, title);
+                }
+              },
+            );
+          },
+          onLoadStop: (controller, url) async {
+            await _setTokenInWebView();
+            await controller.evaluateJavascript(source: """
+              window.FlutterChannel = {
+                postMessage: function(data) {
+                  window.flutter_inappwebview.callHandler('FlutterChannel', data);
+                }
+              };
+            """);
+          },
+        ),
       ),
     );
   }
 
-Future<void> downloadAndSharePdf(String url, String filename) async {
-  try {
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode != 200) {
-      throw Exception('Failed to download file');
-    }
-
-    final tempDir = await getTemporaryDirectory();
-    final filePath = '${tempDir.path}/$filename.pdf';
-    final file = File(filePath);
-    await file.writeAsBytes(response.bodyBytes);
-
-    final xFile = XFile(filePath, name: "$filename.pdf", mimeType: 'application/pdf');
-    await Share.shareXFiles([xFile], text: "Sharing PDF: $filename");
-
-  } catch (e) {
-    print("Error downloading or sharing PDF: $e");
+  Future<void> _setTokenInWebView() async {
+    await _webViewController.evaluateJavascript(source: """
+      localStorage.setItem('headers',JSON.stringify({
+        "authorization": "Bearer <value>",
+        "x-auth-token": "<value>",
+        "x-authenticated-user-token": "<value>",
+        "x-channel-id": "<value>",
+        "x-device-id": "<value>",
+        "x-session-id": "<value>"
+      }));
+      localStorage.setItem('name', <value>);
+      localStorage.setItem('accToken',<value>);
+      localStorage.setItem('profileData', JSON.stringify({
+        "state": "<value>",
+        "cluster": "<value>",
+        "district": "<value>",
+        "block": "<value>",
+        "school": "<value>",
+        "role": "<value>"
+      }));
+    """);
   }
-}
 
+  Future<void> downloadAndSharePdf(String url, String filename) async {
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode != 200) throw Exception("Download failed");
 
-Future<void> downloadFileToDownloads(String url, String filename) async {
-  try {
-    // if (Platform.isAndroid) {
-    //   var status = await Permission.storage.request();
-    //   if (!status.isGranted) {
-    //     throw Exception("Storage permission not granted");
-    //   }
-    // }
+      final dir = await getTemporaryDirectory();
+      final path = '${dir.path}/$filename.pdf';
+      final file = File(path)..writeAsBytesSync(response.bodyBytes);
 
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode != 200) {
-      throw Exception('Failed to download file');
+      final xFile = XFile(path, name: "$filename.pdf", mimeType: 'application/pdf');
+      await Share.shareXFiles([xFile], text: "Sharing PDF: $filename");
+    } catch (e) {
+      print("Download/share error: $e");
     }
-
-    Directory? downloadsDir;
-    if (Platform.isAndroid) {
-      downloadsDir = Directory('/storage/emulated/0/Download');
-    } else {
-      downloadsDir = await getApplicationDocumentsDirectory();
-    }
-
-    final filePath = '${downloadsDir.path}/$filename.pdf';
-    final file = File(filePath);
-
-    await file.writeAsBytes(response.bodyBytes);
-
-    print("File saved to: $filePath");
-  } catch (e) {
-    print("Error saving file: $e");
   }
-}
 
+  Future<void> downloadFileToDownloads(String url, String filename) async {
+    try {
+      if (Platform.isAndroid) {
+        var status = await Permission.storage.request();
+        if (!status.isGranted) throw Exception("Storage permission not granted");
+      }
+
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode != 200) throw Exception("Download failed");
+
+      Directory? downloadDir;
+      if (Platform.isAndroid) {
+        downloadDir = Directory('/storage/emulated/0/Download');
+      } else {
+        downloadDir = await getApplicationDocumentsDirectory();
+      }
+
+      final path = '${downloadDir.path}/$filename.pdf';
+      final file = File(path)..writeAsBytesSync(response.bodyBytes);
+
+      print("Saved to: $path");
+    } catch (e) {
+      print("File save error: $e");
+    }
+  }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_inappwebview_macos
 import path_provider_foundation
 import share_plus
-import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
-  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
